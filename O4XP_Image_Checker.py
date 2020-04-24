610acffba9e9b40060d5c2a277d37a6ebea6a4a0
Eray Yilmaz <mail@erayyilmaz.com>
2020-04-24 09:46:25 -0500

    Update O4XP_Image_Checker.py

--- O4XP_Image_Checker.py
+++ O4XP_Image_Checker.py
@@ -55,51 +55,86 @@ def work(path):
         wrt = str(path)+'\n'
         f.write(wrt)
         f.close()
-        if delete_file == 1:
+        if delete_file == True:
             os.remove(path)
             print('CORRUPT IMAGE LOGGED & DELETED: ' + str(path))
-        if delete_file == 0:
+        if delete_file == False:
             print('CORRUPT IMAGE LOGGED: ' + str(path))
 
 
 def main():
-    # Ask the user if they wish to delete the bad jpg also
-    user_input = '';
     acceptable_input_list = ['yes','no']
-    while user_input not in acceptable_input_list:
-        user_input = input("Do you wish to DELETE corrupt jpg files automatically? (yes/no)").lower()
 
-    if user_input == "yes":
+    cpu_count = int(os.cpu_count())
+
+    user_input_1 = '';
+    print ('------------------------------------------------------------------------------------------------------------------------------')
+    print ('I have determined there are ' + str(cpu_count) + ' available logical cores.')
+
+    while user_input_1 not in acceptable_input_list:
+        user_input_1 = input("Do you wish to use all available cores for maximum performance? (yes/no)").lower()
+    if user_input_1 == "yes":
+        print ('')
+        print ('I will run at full capacity. Other applications may be unusable while I am running.')
+        print ('')
+        workers = cpu_count
+    else:
+        workers = round(cpu_count/3)
+        print ('')
+        print ('I will run at 1/3rd capacity. Using only ' + str(workers) + ' logical cores out of total ' + str(cpu_count))
+        print ('This will be slower however other applications can be run without perfoirmance degradation.')
+        print ('------------------------------------------------------------------------------------------------------------------------------')
+        print ('')
+
+    # Ask the user if they wish to delete the bad jpg also
+    user_input_2 = '';
+    while user_input_2 not in acceptable_input_list:
+        user_input_2 = input("Do you wish to DELETE corrupt jpg files automatically? (yes/no)").lower()
+    if user_input_2 == "yes":
+        print ('')
         print ('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         print ('Any jpg files containing white space will be deleted without confirmation!')
         print ('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-        time.sleep(5)
-        delete_file = 1;
+        print ('')
+        delete_file = True;
     else:
-        delete_file = 0;
+        print ('')
+        delete_file = False;
 
     # Craft a list of each jpg files to be checked.
     image_files = []
     path = pathlib.Path().absolute()
     for z in path.rglob('*.jpg'):
         image_files.append(z)
-    for z in path.rglob('*.jpeg'):
-        image_files.append(z)
 
-    print ('All corrupt jpg files will be listed in checker_log.txt file.')
+    file_count = len(image_files)
+    time_estimate = round(((file_count*2.5)/workers)/60)
+    if time_estimate <= 0:
+        time_estimate = "less than one"
+
+    print ('------------------------------------------------------------------------------------------------------------------------------')
+    print ('There are ' + str(file_count) + ' image files to check.')
+    print ('This operation will take approximately ' + str(time_estimate) + ' minute(s).')
+    print ('Starting in five seconds >>>>>')
     time.sleep(5)
 
-    # for image_file in image_files:
-    #   work(image_file)
-    # return
+    start = time.time()
 
-    # Set up multiprocessing to be equal of the logical CPU unit count for better effeciency.
-    workers = int(os.cpu_count())
+    # Set up multiprocessing.
     with Pool(workers) as p:
         p.imap_unordered(work, image_files)
         p.close()
         p.join()
-        print("ALL FILES CHECKED.")
+        print("All image files have been checked.")
+
+    end = time.time()
+    time_elapsed = round((end - start) / 60)
+    if time_elapsed <= 0:
+        time_elapsed = "less than one"
+
+    print ('------------------------------------------------------------------------------------------------------------------------------')
+    print ('It took ' + str(time_elapsed) + ' minute(s) to check ' + str(file_count) + ' image files.')
+    print ('------------------------------------------------------------------------------------------------------------------------------')
 
 if __name__ == "__main__":
     main()
